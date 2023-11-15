be5363d745fb3e40d54e149b484b46328dd4532b
jabber <2898117012@qq.com>
2023-11-15 23:59:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Shopeo Elementor Plugin Scaffold
+
+
+## Depend Tools
+
+https://github.com/TGMPA/TGM-Plugin-Activation
---
+++ shopeo-elementor-plugin-scaffold.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Plugin Name: SHOPEO Elementor Plugin Scaffold
+ * Plugin URI: https://wordpress.org/plugins/shopeo-elementor-plugin-scaffold
+ * Description:
+ * Author: Shopeo
+ * Version: 0.0.1
+ * Author URI: https://shopeo.cn
+ * License: GPL3+
+ * Text Domain: shopeo-elementor-plugin-scaffold
+ * Domain Path: /languages
+ * Requires at least: 5.9
+ * Requires PHP: 5.6
+ */
+
+if (!defined('ABSPATH')) {
+	exit; // Exit if accessed directly.
+}
+
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+	require_once 'vendor/autoload.php';
+}
+
+if (!defined('SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE')) {
+	define('SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE', __FILE__);
+}
+
+if (!function_exists('shopeo_elementor_plugin_scaffold_activation')) {
+	function shopeo_elementor_plugin_scaffold_activation()
+	{
+
+	}
+}
+
+register_activation_hook(SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE, 'shopeo_elementor_plugin_scaffold_activation');
+
+if (!function_exists('shopeo_elementor_plugin_scaffold_deactivation')) {
+	function shopeo_elementor_plugin_scaffold_deactivation()
+	{
+
+	}
+}
+
+register_deactivation_hook(SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE, 'shopeo_elementor_plugin_scaffold_deactivation');
+
+if (!function_exists('shopeo_elementor_plugin_scaffold_init')) {
+	function shopeo_elementor_plugin_scaffold_init()
+	{
+
+		//load text domain
+		load_plugin_textdomain('shopeo-elementor-plugin-scaffold', false, dirname(plugin_basename(SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE)) . '/languages');
+	}
+}
+
+add_action('init', 'shopeo_elementor_plugin_scaffold_init');
+
+add_action('admin_enqueue_scripts', function () {
+	$plugin_version = get_plugin_data(SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE)['Version'];
+	//style
+
+	//script
+	wp_enqueue_script('shopeo-elementor-plugin-scaffold-admin-script', plugins_url('/assets/js/admin.js', SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE), array('jquery'), $plugin_version);
+	wp_localize_script('shopeo-elementor-plugin-scaffold-admin-script', 'shopeo_elementor_plugin_scaffold', array(
+		'ajax_url' => admin_url('admin-ajax.php')
+	));
+});
+
+add_action('wp_enqueue_scripts', function () {
+	$plugin_version = get_plugin_data(SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE)['Version'];
+	//style
+	wp_enqueue_style('shopeo-elementor-plugin-scaffold-style', plugins_url('/assets/css/style.css', SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE), array(), $plugin_version);
+	wp_style_add_data('shopeo-elementor-plugin-scaffold-style', 'rtl', 'replace');
+
+	//script
+	wp_enqueue_script('shopeo-elementor-plugin-scaffold-script', plugins_url('/assets/js/app.js', SHOPEO_PLUGIN_SCAFFOLD_PLUGIN_FILE), array('jquery'), $plugin_version);
+	wp_localize_script('shopeo-elementor-plugin-scaffold-script', 'shopeo_elementor_plugin_scaffold', array(
+		'ajax_url' => admin_url('admin-ajax.php')
+	));
+});
